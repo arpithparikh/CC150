a8feac30bbf4f2a6d8d7d177d97f103ccfb1ada8
JamesJiG777 <jamesjitough@gmail.com>
2015-03-31 14:40:32 -0400

    03/31/2015

---
+++ CH1 Arrays&Strings/1.5.java
@@ -0,0 +1,28 @@
+// Implement a method to perform basic string compression using the counts of repeated characters.
+//  For example, the string aabcccccaaa would become a2blc5a3. 
+//  If the “compressed” string would not become smaller than the original string, 
+//  your method should return the original string.
+public class Solution{
+	public String compressString(string str){
+		StringBuffer res = new StringBuffer();
+		char[] str = str.toCharArray();
+		char check = str1[0];
+		int count = 1;
+		for(i = 1; i < str.length; i++)
+		{
+			if(str[i] == check)
+				count++;
+			else
+			{
+				res.append((check + ""), count);
+				check = str[i];
+				count = 1;
+			}
+		}
+		res.append((check + ""), count);
+		if(res.toString().length() > str.length)
+			return str;
+		else
+			return res;
+	}
+}
