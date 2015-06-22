8aa20f9933b9e18a3a7d5ab9b2f1c5cb109cbdb8
JamesJiG777 <jamesjitough@gmail.com>
2015-06-22 10:28:30 -0400

    06/22/2015

--- CH2 LinkedLists/2.2.java
+++ CH2 LinkedLists/2.2.java
@@ -1,22 +1,46 @@
 //implement an algorithm to find te kth to last element of a singly linked list
 //recursive solutions are often clear but less optimal
-public class Solution{
-	public int kthtoLast(LinkedListNode head, int k){
-		if (k < 0|| head == null)
+class ListNode {
+	int val;
+	ListNode next;
+	ListNode(int x){
+		val = x;
+	}
+}
+public class Solution {
+	
+	public static void main(String[] args) {
+		
+		ListNode node1 = new ListNode(1);
+		ListNode node2 = new ListNode(2);
+		ListNode node3 = new ListNode(3);
+		ListNode node4 = new ListNode(4);
+		ListNode node5 = new ListNode(5);
+		ListNode node6 = new ListNode(6);
+
+		node1.next = node2;
+		node2.next = node3;
+		node3.next = node4;
+		node4.next = node5;
+		node5.next = node6;
+
+		ListNode head = node1;
+		System.out.println(Solution(head,1));
+
+	}
+	
+	public static int Solution(ListNode head, int k){
+		if(head == null || k <0)
 			return -1;
-		LinkedListNode p1 = head;
-		LinkedListNode p2 = head;
-		//moving p2 to reach the position length - k.
-		for(int i = 0; i< k - 1; i++)
+		ListNode fast = head;
+		ListNode slow = head;
+		for(int i =0;i<k-1;i++)
+			fast = fast.next;
+		while(fast.next!= null)
 		{
-			p2 = p2.next;
+			slow = slow.next;
+			fast = fast.next;
 		}
-		//p1 and p2 are moving together
-		while(p2.next != null)
-		{
-			p1 = p1.next;
-			p2 = p2.next;
-		}
-		return p1;
+		return slow.val;
 	}
 }
