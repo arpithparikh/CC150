b5df31f4ce1fdbdb4241732bc836d61fcb3c297d
JamesJiG777 <jamesjitough@gmail.com>
2015-06-22 14:52:47 -0400

    06/22/2015

--- CH2 LinkedLists/2.6.java
+++ CH2 LinkedLists/2.6.java
@@ -1,4 +1,100 @@
+/*
+同Leetcode #142	Linked List Cycle II
+Given a circular linked list, implement an algorithm which returns the node at the beginning of the loop.
+DEFINITION
+Circular linked list: A (corrupt) linked list in which a node's next pointer points to an earlier node, 
+so as to make a loop in the linked list.
+EXAMPLE
+Input:A ->B->C->D->E-> C[the same C as earlier] 
+Output:C
+*/
 //Given a circular linked list, implement an algorithm which returns the node at the begining of the loop
+//time O(n), space O(1)
+import java.util.Stack;
+
+class ListNode {
+	int val;
+	ListNode next;
+	ListNode(int x){
+		val = x;
+	}
+}
+public class Solution {
+	
+	private static ListNode arrayToLinkedList(int[] arr) {
+		if (arr == null || arr.length < 1) return null;
+		ListNode fakeHead = new ListNode(0);
+		ListNode cur = fakeHead;
+		for(int i = 0; i < arr.length; i++) {
+			cur.next = new ListNode(arr[i]);
+			cur = cur.next;
+		}
+		return fakeHead.next;
+	}
+	
+	private static boolean compareLinkedList(ListNode a, ListNode b) {
+		ListNode p1 = a;
+		ListNode p2 = b;
+		while (p1 != null && p2 != null) {
+			if (p1.val != p2.val) {
+				return false;
+			} else {
+				p1 = p1.next;
+				p2 = p2.next;
+			}
+		}
+		return p1 == null && p2 == null;
+	}
+
+	public static ListNode Solution(ListNode head){
+		if(head == null || head.next == null || head.next.next == null)
+			return null;
+		ListNode fast = head;
+		ListNode slow = head;
+		while(fast.next!= null)
+		{
+			fast = fast.next.next;
+			slow = slow.next;
+			if(fast == slow)
+				break;
+		}
+		slow = head;
+		while(slow!=fast)
+		{
+			slow = slow.next;
+			fast = fast.next;
+		}
+		return slow;
+	}
+
+
+
+	public static void main(String[] args) {	
+		
+		ListNode n1 = new ListNode(1);
+		ListNode n2 = new ListNode(2); n1.next = n2;
+		ListNode n3 = new ListNode(3); n2.next = n3;
+		ListNode n4 = new ListNode(4); n3.next = n4;
+		ListNode n5 = new ListNode(5); n4.next = n5;
+		n5.next = n3;
+		ListNode head1 = n1;
+		System.out.println( Solution(head1) == n3 );
+
+		ListNode n11 = new ListNode(1);
+		ListNode n22 = new ListNode(2); n11.next = n22;
+		ListNode n33 = new ListNode(3); n22.next = n33;
+		n33.next = n33;
+		ListNode head11 = n11;
+		System.out.println( Solution(head11) == n33 );
+
+		ListNode n111 = new ListNode(1);
+		n111.next = n111;
+		ListNode head111 = n11;
+		System.out.println( Solution(head111) == n111 );
+			
+	}		
+}
+
 public class Solution{
 	public LinkedListNode findBegining(LinkedListNode head){
 		LinkedListNode slow = head;
--- CH2 LinkedLists/2.7.java
+++ CH2 LinkedLists/2.7.java
@@ -1,53 +1,174 @@
 //is valid palindrome
-//reverse and compare, time complexity O(n), space complexity O(n)
-public class Solution1{
-	public boolean isValidPalindrome(LinkedListNode head){
-		LinkedListNode l1 = head;
-		LinkedListNode l2 = head;
-		while(l1 != null && l1.next != null)
-		{
-			LinkedListNode before = new LinkedListNode;
-			before.next = l2;
-			before.data = l1.next.data;
-			l1 = l1.next;
-			l2 = before;
-		}
-		while( l1 != null || l1.next != null)
-		{
-			if(l1.data == l2.data)
-			{
-				l1 = l1.next;
-				l2 = l2.next;
-			}
-			return true;
-		}
-		return false;
+//reverse and compare, time complexity O(n), space complexity O(n), iterative
+class ListNode {
+	int val;
+	ListNode next;
+	ListNode(int x){
+		val = x;
 	}
 }
-//Iterative approach, time complexity O(n), space complexity O(n)
-public class Solution2{
-	public boolean isValidPalindrome(LinkedListNode head){
-		LinkedListNode fast = head;
-		LinkedListNode slow = head;
-		Stack<Integer> res = new Stack<Integer>();
-		while(fast != null || fast.next != null)
-		{
-			res.push(slow.data);
-			slow = slow.next;
-			fast = fast.next.next;
+public class Solution {
+	
+	private static ListNode arrayToLinkedList(int[] arr) {
+		if (arr == null || arr.length < 1) return null;
+		ListNode fakeHead = new ListNode(0);
+		ListNode cur = fakeHead;
+		for(int i = 0; i < arr.length; i++) {
+			cur.next = new ListNode(arr[i]);
+			cur = cur.next;
 		}
-		//if odd number, skip
-		if(fast != null)
-			slow = slow.next;
-		while(slow != null)
-		{
-			//The Object returned by l.get(i) is an instance of the Integer class.
-			//intValue() is a instance method of the Integer class that returns a primitive int.
-			int top = res.pop().intValue();
-			if(top != slow.data)
+		return fakeHead.next;
+	}
+	
+	private static boolean compareLinkedList(ListNode a, ListNode b) {
+		ListNode p1 = a;
+		ListNode p2 = b;
+		while (p1 != null && p2 != null) {
+			if (p1.val != p2.val) {
 				return false;
-			slow = slow.next;
+			} else {
+				p1 = p1.next;
+				p2 = p2.next;
+			}
+		}
+		return p1 == null && p2 == null;
+	}
+
+
+
+	public static boolean Solution(ListNode head){
+		if(head == null)
+			return true;
+		if(head.next == null)
+			return true;
+		Stack<Integer> stack1 = new Stack<Integer>();
+		ListNode fakeHead = head;
+		while(fakeHead != null)
+		{
+			stack1.push(fakeHead.val);
+			fakeHead = fakeHead.next;
+		}
+		while(!stack1.isEmpty())
+		{
+			int temp = stack1.pop();
+			if(temp == head.val)
+				head = head.next;
+			else
+				return false;
 		}
 		return true;
 	}
+
+
+
+	public static void main(String[] args) {	
+		
+		ListNode test0 = null;		
+		ListNode test1 = arrayToLinkedList(new int[]{1});
+		ListNode test2 = arrayToLinkedList(new int[]{20,10,20});
+		ListNode test3 = arrayToLinkedList(new int[]{1,2,3,4,5,4,3,2,1});
+		ListNode test4 = arrayToLinkedList(new int[]{2,2,2});
+		ListNode test5 = arrayToLinkedList(new int[]{-5,2,4,0,-7,-6,1,9,-10,12});
+		ListNode test6 = arrayToLinkedList(new int[]{2,3,4,2,1});
+		
+		System.out.println(Solution(test0));
+		System.out.println(Solution(test1));
+		System.out.println(Solution(test2));
+		System.out.println(Solution(test3));
+		System.out.println(Solution(test4));
+		System.out.println(Solution(test5));
+		System.out.println(Solution(test6));
+		
+	}		
+}
+
+//recursive approach, time complexity O(n), space complexity O(n)
+package CC150;
+
+import java.util.ArrayList;
+
+class ListNode {
+	int val;
+	ListNode next;
+	ListNode(int x){
+		val = x;
+	}
+}
+public class Solution {
+	
+	private static ListNode arrayToLinkedList(int[] arr) {
+		if (arr == null || arr.length < 1) return null;
+		ListNode fakeHead = new ListNode(0);
+		ListNode cur = fakeHead;
+		for(int i = 0; i < arr.length; i++) {
+			cur.next = new ListNode(arr[i]);
+			cur = cur.next;
+		}
+		return fakeHead.next;
+	}
+	
+	private static boolean compareLinkedList(ListNode a, ListNode b) {
+		ListNode p1 = a;
+		ListNode p2 = b;
+		while (p1 != null && p2 != null) {
+			if (p1.val != p2.val) {
+				return false;
+			} else {
+				p1 = p1.next;
+				p2 = p2.next;
+			}
+		}
+		return p1 == null && p2 == null;
+	}
+
+	
+
+	public static boolean Solution(ListNode head){
+		if(head == null||head.next == null)
+			return true;
+		ArrayList<Integer> list1 = new ArrayList<Integer>();
+		ArrayList<Integer> list2 = new ArrayList<Integer>();
+		ListNode temp = head;
+		while(temp.next!= null)
+		{
+			list1.add(temp.val);
+			temp = temp.next;
+		}
+		helper(head, list2);
+		for(int i =0;i<list1.size();i++)
+		{
+			if(list2.get(i)!= list1.get(i))
+				return false;
+		}
+		return true;
+	}
+	private static void helper(ListNode head, ArrayList<Integer> list){
+		if(head == null)
+			return;
+		helper(head.next, list);
+		list.add(head.val);
+		return;
+	}
+
+
+
+	public static void main(String[] args) {	
+		
+		ListNode test0 = null;		
+		ListNode test1 = arrayToLinkedList(new int[]{1});
+		ListNode test2 = arrayToLinkedList(new int[]{20,10,20});
+		ListNode test3 = arrayToLinkedList(new int[]{1,2,3,4,5,4,3,2,1});
+		ListNode test4 = arrayToLinkedList(new int[]{2,2,2});
+		ListNode test5 = arrayToLinkedList(new int[]{-5,2,4,0,-7,-6,1,9,-10,12});
+		ListNode test6 = arrayToLinkedList(new int[]{2,3,4,2,1});
+		
+		System.out.println(Solution(test0));
+		System.out.println(Solution(test1));
+		System.out.println(Solution(test2));
+		System.out.println(Solution(test3));
+		System.out.println(Solution(test4));
+		System.out.println(Solution(test5));
+		System.out.println(Solution(test6));
+		
+	}		
 }
