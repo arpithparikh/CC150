dc2288021ed61866281a50663487977840022c9e
JamesJiG777 <jamesjitough@gmail.com>
2015-06-25 16:54:07 -0400

    06/25/2015

--- CH1 Arrays&Strings/1.4.java
+++ CH1 Arrays&Strings/1.4.java
@@ -1,93 +1,46 @@
 //Write a method to replace all spaces in a string with '%20'
 //Bacause the String in Java is immutable, so we will use characters array
-public class Solution{
-	public void replaceSpace(char[] str, int length){
-		if(str == null || str.length == 0)
-			return;
-		int count =0;
-		for(int i = 0;i <str.length;i++)
+package CC150;
+public class Solution {
+	 
+	public static void main(String[] args) {
+		//assume  input string is not null and have enough space to 
+		char[] str = {'m','r',' ',' ','m','r',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '};
+		String result = Solution(str,6);
+		System.out.println(result);
+		 str = new char[] {'m','r',' ',' ','m','r','m','r',' ','m','r',' ','m','r',' ','m','r',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '};
+		 result = Solution(str,17);
+		System.out.println(result);
+		 str = new char[] {'m','r',' ',' ',' ','m','r','m','r',' ','m','r',' ','m','r',' ','m','r',' ',' ','s','m','i','t','h',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '};
+		 result = Solution(str,25);
+		System.out.println(result);
+	}
+	private static String Solution(char[] string,int length) {
+		if(string == null || string.length==0)
+			return String.valueOf(string);
+		int count = 0;
+		for(int i =0;i<length;i++)
 		{
-			if(str[i] == ' ')
+			if(string[i] == ' ')
 				count++;
 		}
-		int newLength = str.length + 2*count;
-		for(int i = str.length -1; i>=0;i--)
-		{
-			if(str[i] == ‘ ’)
-			{
-				str[newLength - 1] = '0';
-				str[newLength - 2] = '2';
-				str[newLength - 3] = '%';
-				newLength = newLength - 3;
-			}
-			else
-			{
-				str[newLength - 1] = str[i];
-				newLength = newLength - 1;
-			}
-		}
-	}
-}
-public class Solution{//相当于将原来的char【】扩容后，然后再从原来end开始从后往前扫描，边扫边往后面添加元素
-	public void replaceSpace(char[] string, int length){
-		if(string == null || string.length == 0)
-			return;
-		int count = 0;
-		length = string.length ;
-		for(int i =0; i<length;i++)
-		{
-			if(string[i] == ' ');
-			count++;
-		}
+		char[] res = new char[length + 2*count];
 		int newLength = length + 2*count;
-		for(int i = length -1; i>=0;i--)
+		for(int i = length-1;i>=0;i--)
 		{
-			if(string[i] != ' ')
+			if(string[i] == ' ')
 			{
-				string[newLength -1] = string[i];
-				newLength = newLength -1;//注意这个地方length的变化
+				res[newLength-1] = '0';
+				res[newLength-2] = '2';
+				res[newLength-3] = '%';
+				newLength = newLength-3;
 			}
-			else
-			{
-				string[newLength - 1] = '0';
-				string[newLength - 2] = '2';
-				string[newLength - 3] = '%';
-				newLength = newLength -3;//注意这个地方length的变化
+			
+			else{
+				res[newLength-1] = string[i];
+				newLength = newLength-1;
 			}
 		}
-		return;
-	}
-}
-
-
-
-
-public class Solution{
-	public void replaceSpace(char[] string, int length){
-		if(string == null || string.length == 0)
-			return;
-		int count = 0;
-		for(int i =0;i<string.length;i++)
-		{
-			if(string[i] == ' ');
-			count++;
-		}
-		int length = string.length + count*2;
-		for(int i =str.length-1;i>=0;i--)
-		{
-			if(string[i] =' ')
-			{
-				string[length-1] = '0';
-				string[length-2] = '2';
-				string[length-3] = '%';
-				length-=3;
-			}
-			else
-			{
-				string[length-1] = string[i];
-				length-=1;
-			}
-		}
-		return;
+		return String.valueOf(res);
 	}
 }
--- CH1 Arrays&Strings/1.5.java
+++ CH1 Arrays&Strings/1.5.java
@@ -2,27 +2,43 @@
 //  For example, the string aabcccccaaa would become a2blc5a3. 
 //  If the “compressed” string would not become smaller than the original string, 
 //  your method should return the original string.
-public class Solution{
-	public String compressString(string str){
-		StringBuffer res = new StringBuffer();
-		char[] str = str.toCharArray();
-		char check = str[0];
+package CC150;
+public class Solution {
+	
+	public static String compress(String str) {
+		StringBuilder res = new StringBuilder();
+		char[] str1 = str.toCharArray();
+		char check = str1[0];
 		int count = 1;
-		for(i = 1; i < str.length; i++)
-		{
-			if(str[i] == check)
+		for(int i =1;i<str1.length;i++){
+			if(str1[i] == check)
 				count++;
 			else
 			{
-				res.append((check + ""), count);
-				check = str[i];
+				res.append(check+"");
+				res.append(count);
+				check = str1[i];
 				count = 1;
 			}
 		}
-		
-		if(res.toString().length() > str.length)
+		if(res.toString().length() > str.length())
 			return str;
 		else
-			return res;
+			return res.toString();
 	}
+ 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String test = "aabcccccaaa";
+		System.out.println(compress(test));
+		String test1 = "       ";
+		System.out.println(compress(test1));
+		String test2 = "zhangxinshidiaomiMa";
+		System.out.println(compress(test2));
+		String test3 = "TaShiDiaoBi";
+		System.out.println(compress(test3));
+		String test4 = "aaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbbbbbbssssssssssssssssssssssssssssssssssssssssssssssssss";
+		System.out.println(compress(test4));
+	}
+ 
 }
