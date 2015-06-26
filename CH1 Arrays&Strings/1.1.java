7966535f18fe2d524fa59bcb6e78667d797eea7c
JamesJiG777 <jamesjitough@gmail.com>
2015-06-25 22:00:58 -0400

    6/25/2015

--- CH1 Arrays&Strings/1.1.java
+++ CH1 Arrays&Strings/1.1.java
@@ -8,18 +8,86 @@
 //the length of array is array.length
 //array[i], str.charAt(i). array=>[], string => ()
 //time complexity is O(n)
-public class Solution{
-	public boolean isUnique(String str){
+package CC150;
+
+import java.util.Arrays;
+
+public class Solution {
+	public static boolean Solution(String str){
+		
 		if(str == null || str.length()<0||str.length()>128)
 			return false;
-		boolean check = new boolean[256];
+		boolean[] check = new boolean[256];
 		for(int i =0;i<str.length();i++)
 		{
-			if(boolean[str.charAt(i)])
+			if(check[str.charAt(i)])
 				return false;
 			else
 				check[str.charAt(i)] = true;
 		}
 		return true;
 	}
+
+	public static void main(String[] args) {	
+		String s0 = null;
+		String s1 = "";
+		String s2 = "abcdefg";
+		String s3 = "abcadg";
+		String s4 = "z";
+		String s5 = "$@)+~e";
+		String s6 = "  ";
+		String s7 = " ad4-+";
+
+		System.out.println(Solution(s0)); 
+		System.out.println(Solution(s1)); 
+		System.out.println(Solution(s2)); 
+		System.out.println(Solution(s3)); 
+		System.out.println(Solution(s4)); 
+		System.out.println(Solution(s5)); 
+		System.out.println(Solution(s6)); 
+		System.out.println(Solution(s7)); 
+	}		
 }
+
+//time complexity O(nlogn), sort first and then compare adjacent element to check whether they are the same
+package CC150;
+
+import java.util.Arrays;
+
+public class Solution {
+	public static boolean Solution(String str){
+		if(str == null || str.length()<0||str.length()>128)
+			return false;
+		char[] res = str.toCharArray();
+		Arrays.sort(res);
+		for(int i =0;i<res.length-1;i++)
+		{
+			if(res[i] == res[i+1])
+				return false;
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {	
+		String s0 = null;
+		String s1 = "";
+		String s2 = "abcdefg";
+		String s3 = "abcadg";
+		String s4 = "z";
+		String s5 = "$@)+~e";
+		String s6 = "  ";
+		String s7 = " ad4-+";
+
+		System.out.println(Solution(s0)); 
+		System.out.println(Solution(s1)); 
+		System.out.println(Solution(s2)); 
+		System.out.println(Solution(s3)); 
+		System.out.println(Solution(s4)); 
+		System.out.println(Solution(s5)); 
+		System.out.println(Solution(s6)); 
+		System.out.println(Solution(s7)); 
+	}		
+}
+
+
+
