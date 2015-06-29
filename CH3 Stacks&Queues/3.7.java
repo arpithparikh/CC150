865c2d8b8c487a8460ecb40c0cdf1a021d987b08
JamesJiG777 <jamesjitough@gmail.com>
2015-06-28 23:34:07 -0400

    06/28/2015

--- CH3 Stacks&Queues/3.7.java
+++ CH3 Stacks&Queues/3.7.java
@@ -1,4 +1,8 @@
-//cats and dogs,
+// An animal shelter holds only dogs and cats, and operates on a strictly "first in, first out"
+// basis. People must adopt either the "oldest" (based on arrival time) of all animals at the shelter, or they can select
+// whether they would prefer a dog or a cat(and will receive the oldest animal of that type). They cannot select which specific animal thet would
+// like. Create the data structures to maintain this system and implement operation such as an enqueue, dequeueAny, dequeueDog and dequeueCats. You
+// may use built-in LinkedList data structure
 public class Solution{
 	private int order;
 	protected String name;
